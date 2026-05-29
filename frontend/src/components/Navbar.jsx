ca48d817a6637b8b62b0dec7f9879a84ed6f43e6
Dylan <dylansinghabahu360@gmail.com>
2026-05-29 18:06:09 +1000

    initialise auth token by retrieving localstorage token

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,5 +1,7 @@
 import {Link} from "react-router-dom";
 import useLogout from "../hooks/useLogout.jsx";
+import { useContext } from "react";
+import { authenticateContext } from "../context/authenticateContext";
 
 const Navbar = () => {
     const { logout } = useLogout();
@@ -7,21 +9,34 @@ const Navbar = () => {
         logout();
     }
 
+    const { user } = useContext(authenticateContext);
+
     return (
         <header>
             <div className="navbar">
                 <Link to="/" className="navbar-link">
                     Admin
                 </Link>
-                <Link to="/login" className="navbar-link">
-                    Login
-                </Link>    
-                <Link to="/signup" className="navbar-link">
-                    Sign Up
-                </Link>   
-                <button onClick={logoutHandler} className="navbar-link">
-                    Logout
-                </button>
+                <nav>
+                    {!user && (
+                        <>
+                            <Link to="/login" className="navbar-link">
+                                Login
+                            </Link>    
+                            <Link to="/signup" className="navbar-link">
+                                Sign Up
+                            </Link>   
+                        </>
+                    )}
+                    {user && (
+                        <div>
+                            <span>{user.email}</span>
+                            <button onClick={logoutHandler} className="navbar-link">
+                                Logout
+                            </button>
+                        </div>
+                    )}
+                </nav>
             </div>
         </header>
     )
